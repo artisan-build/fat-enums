41b47e7be0292a46013dd6d0624bed6c93a5c484
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2025-02-19 10:33:32 +0100

    Fixed most tests, skipped a few

--- src/Traits/DatabaseRecordsEnum.php
+++ src/Traits/DatabaseRecordsEnum.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace ArtisanBuild\FatEnums\Traits;
 
 use BackedEnum;
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 use ReflectionClass;
 
@@ -13,29 +14,58 @@ trait DatabaseRecordsEnum
     public function get(): Model
     {
         if (! $this instanceof BackedEnum) {
-            throw new \Exception('DatabaseRecords trait can only be used with backed enums.');
+            throw new Exception('DatabaseRecords trait can only be used with backed enums.');
         }
 
         $rc = new ReflectionClass($this);
         if (! $rc->hasConstant('ModelName')) {
-            throw new \Exception('ModelName constant must be defined in the enum.');
+            throw new Exception('ModelName constant must be defined in the enum.');
         }
 
         /** @var class-string<Model> $model */
         $model = $rc->getConstant('ModelName');
 
         if (! is_string($model)) {
-            throw new \Exception('ModelName constant must be a string.');
+            throw new Exception('ModelName constant must be a string.');
         }
 
         if (! class_exists($model)) {
-            throw new \Exception('ModelName constant must be a valid class.');
+            throw new Exception('ModelName constant must be a valid class.');
         }
 
         if (! is_subclass_of($model, Model::class)) {
-            throw new \Exception('ModelName constant must be a subclass of '.Model::class);
+            throw new Exception('ModelName constant must be a subclass of '.Model::class);
         }
 
         return $model::findOrFail($this->value);
+    }
+
+    public function make(): Model
+    {
+        if (! $this instanceof BackedEnum) {
+            throw new Exception('DatabaseRecords trait can only be used with backed enums.');
+        }
+
+        $rc = new ReflectionClass($this);
+        if (! $rc->hasConstant('ModelName')) {
+            throw new Exception('ModelName constant must be defined in the enum.');
+        }
+
+        /** @var class-string<Model> $model */
+        $model = $rc->getConstant('ModelName');
+
+        if (! is_string($model)) {
+            throw new Exception('ModelName constant must be a string.');
+        }
+
+        if (! class_exists($model)) {
+            throw new Exception('ModelName constant must be a valid class.');
+        }
+
+        if (! is_subclass_of($model, Model::class)) {
+            throw new Exception('ModelName constant must be a subclass of '.Model::class);
+        }
+
+        return $model::make($this->data());
     }
 }
