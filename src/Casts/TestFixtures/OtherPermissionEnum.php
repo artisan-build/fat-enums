fd0e3d2b29b07c749c676601c5a838b8593bb7c7
Len Woodward <Len@ProjektGopher.com>
2025-05-17 01:32:22 -0700

    add test fixtures

---
+++ src/Casts/TestFixtures/OtherPermissionEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArtisanBuild\FatEnums\Casts\TestFixtures;
+
+enum OtherPermissionEnum: int
+{
+    case READ = 1;
+} 
---
+++ src/Casts/TestFixtures/PermissionEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ArtisanBuild\FatEnums\Casts\TestFixtures;
+
+enum PermissionEnum: int
+{
+    case READ   = 0x1 << 0;
+    case WRITE  = 0x1 << 1;
+    case DELETE = 0x1 << 2;
+    case ADMIN  = 0x1 << 3;
+} 
